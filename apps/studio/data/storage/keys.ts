fd07d847a21b50e6e63956362bbee3345308496e
Ivy Bixler <ivybixler@gmail.com>
2025-08-28 12:28:38 -0400

    working on updating our supabase deps first step: delete all the garbage we use not!!!

--- apps/studio/data/storage/keys.ts
+++
@@ -1,4 +0,0 @@
-export const storageKeys = {
-  buckets: (projectRef: string | undefined) => ['projects', projectRef, 'buckets'] as const,
-  archive: (projectRef: string | undefined) => ['projects', projectRef, 'archive'] as const,
-}
--- apps/studio/data/table-editor/keys.ts
+++
@@ -1,4 +0,0 @@
-export const tableEditorKeys = {
-  tableEditor: (projectRef: string | undefined, id: number | undefined) =>
-    ['projects', projectRef, 'table-editor', id] as const,
-}
