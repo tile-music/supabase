d50fc46d01e6f9ec8f355a24f4a71e2eb396f25c
ivy bixler <ivybixler@gmail.com>
2025-12-07 11:35:12 -0500

    removed test schema

--- docker/volumes/db/init/data.sql
+++ docker/volumes/db/init/data.sql
@@ -7,7 +7,6 @@ ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES 
 ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
 ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
 
-CREATE SCHEMA IF NOT EXISTS test;
 CREATE SCHEMA IF NOT EXISTS prod;
 
 -- Grant permissions on the prod schema
@@ -15,14 +14,11 @@ GRANT USAGE ON SCHEMA prod TO public;
 GRANT CREATE ON SCHEMA prod TO public;
 
 -- Grant permissions on the test schema
-GRANT USAGE ON SCHEMA test TO public;
-GRANT CREATE ON SCHEMA test TO public;
 
 -- Grant object-level permissions for tables in prod schema
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA prod TO public;
 
 -- Grant object-level permissions for tables in test schema
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA test TO public;
 CREATE DOMAIN "prod"."isrc" AS "text" NOT NULL
 	CONSTRAINT "isrc_check" CHECK ((VALUE ~* '^[A-Za-z]{2}-?\w{3}-?\d{2}-?\d{5}$'::"text"));
 ALTER DOMAIN "prod"."isrc" OWNER TO "postgres";
@@ -47,10 +43,8 @@ CREATE TABLE IF NOT EXISTS "prod"."albums"(
     CONSTRAINT noduplicates UNIQUE NULLS NOT DISTINCT (album_name, album_type, num_tracks, release_day,release_month, release_year, artists, genre)
 );
 
-CREATE TABLE test.albums (LIKE prod.albums INCLUDING ALL);
 
 ALTER TABLE "prod"."albums" OWNER TO "postgres";
-ALTER TABLE "test"."albums" OWNER TO "postgres";
 
 
 CREATE TABLE IF NOT EXISTS "prod"."album_mbids" (
@@ -62,12 +56,9 @@ CREATE TABLE IF NOT EXISTS "prod"."album_mbids" (
 	CONSTRAINT album_mbid_key FOREIGN KEY ("album_id") REFERENCES "prod"."albums"("album_id") ON DELETE CASCADE
 );
 
-CREATE TABLE test.album_mbids (LIKE prod.album_mbids INCLUDING ALL);
 
 ALTER TABLE "prod"."album_mbids" OWNER TO "postgres";
-ALTER TABLE "test"."album_mbids" OWNER TO "postgres";
 
-ALTER TABLE "test"."album_mbids" ADD CONSTRAINT album_mbid FOREIGN KEY ("album_id") REFERENCES test.albums("album_id");
 
 -- Tracks
 CREATE TABLE IF NOT EXISTS prod."tracks" (
@@ -83,15 +74,11 @@ CREATE TABLE IF NOT EXISTS prod."tracks" (
     constraint album_id_ref FOREIGN KEY ("album_id") REFERENCES "prod"."albums"("album_id") ON DELETE CASCADE,
     CONSTRAINT noduplicates_1 UNIQUE NULLS NOT DISTINCT ("isrc", "track_name", "track_artists", "track_duration_ms")
 );
-CREATE TABLE test.tracks (LIKE prod.tracks INCLUDING ALL);
-ALTER TABLE test.tracks ADD CONSTRAINT album_id_ref FOREIGN KEY (album_id) REFERENCES test.albums("album_id");
-alter table test.tracks add CONSTRAINT noduplicates_1 UNIQUE NULLS NOT DISTINCT ("isrc", "track_name", "track_artists", "track_duration_ms");
 
 --CREATE UNIQUE INDEX idx_unique_albums
 --ON "prod"."albums" (album_name, album_type, num_tracks, release_day,release_month,release_year, artists, genre, upc, ean, popularity, image);
 
 ALTER TABLE "prod"."tracks" OWNER TO "postgres";
-ALTER TABLE "test"."tracks" OWNER TO "postgres";
 
 CREATE TABLE IF NOT EXISTS "prod"."track_mbids" (
 	"track_id" bigint NOT NULL,
@@ -104,11 +91,6 @@ CREATE TABLE IF NOT EXISTS "prod"."track_mbids" (
 	CONSTRAINT track_album_mbid_key FOREIGN KEY ("album_mbid") REFERENCES "prod"."album_mbids"("mbid") ON DELETE CASCADE
  );
 
-CREATE TABLE test.track_mbids (LIKE prod.track_mbids INCLUDING ALL);
-
-ALTER TABLE test.track_mbids ADD CONSTRAINT track_mbid_key FOREIGN KEY ("track_id") REFERENCES "test"."tracks"("track_id") ON DELETE CASCADE;
-ALTER TABLE test.track_mbids ADD CONSTRAINT track_album_mbid_key FOREIGN KEY ("album_mbid") REFERENCES "test"."album_mbids"("mbid") ON DELETE CASCADE;
-
 create table if not exists "prod"."album_art" (
 	"mbid" uuid not null,
 	"small" text,
@@ -120,10 +102,6 @@ create table if not exists "prod"."album_art" (
 	"type" text not null, 
 	constraint mbid_ref foreign key ("mbid") references "prod".album_mbids("mbid") on delete cascade
 );
-
-create table test.album_art (like prod.album_art including all);
-
-alter table test.album_art add constraint mbid_ref foreign key ("mbid") references "test".album_mbids("mbid");
 
 -- Played Tracks
 create table prod.played_tracks (
@@ -146,25 +124,6 @@ CREATE TABLE prod.unmatched_played_tracks (LIKE prod.played_tracks INCLUDING ALL
 ALTER TABLE prod.unmatched_played_tracks ADD CONSTRAINT track_id_ref FOREIGN KEY (track_id) REFERENCES prod.tracks("track_id");
 alter table prod.unmatched_played_tracks add Constraint user_id_ref_test FOREIGN KEY ("user_id") References "auth".users(id) on delete cascade;
 
-CREATE table test.played_tracks (LIKE prod.played_tracks INCLUDING ALL);
-ALTER TABLE test.played_tracks ADD CONSTRAINT track_id_ref FOREIGN KEY (track_id) REFERENCES test.tracks("track_id");
-alter table test.played_tracks add Constraint user_id_ref_test FOREIGN KEY ("user_id") References "auth".users(id) on delete cascade;
-alter table test.played_tracks add CONSTRAINT mbid_ref foreign key ("selected_mbid") references "prod"."album_mbids"("mbid") on delete cascade;
-
-CREATE table test.unmatched_played_tracks (LIKE prod.played_tracks INCLUDING ALL);
-ALTER TABLE test.unmatched_played_tracks ADD CONSTRAINT track_id_ref FOREIGN KEY (track_id) REFERENCES test.tracks("track_id");
-alter table test.unmatched_played_tracks add Constraint user_id_ref_test FOREIGN KEY ("user_id") References "auth".users(id) on delete cascade;
-
-
--- Table permissions for test & prod
-GRANT USAGE ON SCHEMA test TO anon, authenticated, service_role;
-GRANT ALL ON ALL TABLES IN SCHEMA test TO anon, authenticated, service_role;
-GRANT ALL ON ALL ROUTINES IN SCHEMA test TO anon, authenticated, service_role;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA test TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA test GRANT ALL ON TABLES TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA test GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA test GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
-
 GRANT USAGE ON SCHEMA prod TO anon, authenticated, service_role;
 GRANT ALL ON ALL TABLES IN SCHEMA prod TO anon, authenticated, service_role;
 GRANT ALL ON ALL ROUTINES IN SCHEMA prod TO anon, authenticated, service_role;
@@ -175,7 +134,6 @@ ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA prod GRANT ALL ON SEQUENCES
 
 -- might not need this if we have the above
 ALTER table "prod"."albums" OWNER TO "postgres";
-ALTER table "test"."albums" OWNER TO "postgres";
 
 GRANT ALL ON TABLE "prod"."albums" TO "anon";
 GRANT ALL ON TABLE "prod"."albums" TO "authenticated";
@@ -188,30 +146,6 @@ GRANT ALL ON TABLE "prod"."played_tracks" TO "service_role";
 GRANT ALL ON TABLE "prod"."unmatched_played_tracks" TO "anon";
 GRANT ALL ON TABLE "prod"."unmatched_played_tracks" TO "authenticated";
 GRANT ALL ON TABLE "prod"."unmatched_played_tracks" TO "service_role";
-
-GRANT ALL ON TABLE "prod"."tracks" TO "anon";
-GRANT ALL ON TABLE "prod"."tracks" TO "authenticated";
-GRANT ALL ON TABLE "prod"."tracks" TO "service_role";
-
-GRANT ALL ON TABLE "test"."tracks" TO "anon";
-GRANT ALL ON TABLE "test"."tracks" TO "authenticated";
-GRANT ALL ON TABLE "test"."tracks" TO "service_role";
-
-GRANT ALL ON TABLE "test"."albums" TO "anon";
-GRANT ALL ON TABLE "test"."albums" TO "authenticated";
-GRANT ALL ON TABLE "test"."albums" TO "service_role";
-
-GRANT ALL ON TABLE "test"."played_tracks" TO "anon";
-GRANT ALL ON TABLE "test"."played_tracks" TO "authenticated";
-GRANT ALL ON TABLE "test"."played_tracks" TO "service_role";
-
-GRANT ALL ON TABLE "test"."unmatched_played_tracks" TO "anon";
-GRANT ALL ON TABLE "test"."unmatched_played_tracks" TO "authenticated";
-GRANT ALL ON TABLE "test"."unmatched_played_tracks" TO "service_role";
-
-
-
--- continue albums here for now focus on creds
 
 -- Profiles
 CREATE TABLE IF NOT EXISTS "public"."profiles" (
